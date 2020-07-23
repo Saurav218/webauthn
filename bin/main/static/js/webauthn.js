d810204cb98c6723743562f2d1b8ee2f9a961808
Saurav218 <15jk218@ymklab.net>
2020-07-23 21:06:29 +0900

    add: createCredential
    認証器からAttestationResponseを取得する関数を追加しました

--- bin/main/static/js/webauthn.js
+++ bin/main/static/js/webauthn.js
@@ -31,3 +31,28 @@ function postAttestationOptions() {
         }
     });
 }
+
+function createCredential(options) {
+    // ArrayBufferに変換
+    options.challenge = stringToArrayBuffer(options.challenge.value)
+    options.user.id = stringToArrayBuffer(options.user.id)
+    options.excludeCredentials = options.excludeCredentials
+        .map(credential => Object.assign({}, credential, {
+            id: base64ToArrayBuffer(credential.id)
+        }))
+
+    // 認証器からAttestationResponseを取得するWebAuthn API
+    return navigator.credentials.create({
+        'publicKey': options
+    });
+}
+
+// 文字列をArrayBufferに変換
+function stringToArrayBuffer(string) {
+    return new TextEncoder().encode(string);
+}
+
+// Base64文字列をArrayBufferにデコード
+function base64ToArrayBuffer(base64String) {
+    return Uint8Array.from(atob(base64String), c => c.charCodeAt(0));
+}
